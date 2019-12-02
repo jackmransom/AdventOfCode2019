898c9d16314be956ff7224aaaa5dce404d9c6792
Jack Ransom <jack.ransom@idas.me.uk>
2019-12-02 11:55:04 +0000

    Day 1: simplify

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,40 +1,26 @@
-#include <string>
+#include <cstdio>
 #include <iterator>
 #include <numeric>
 #include <fstream>
 #include <vector>
-#include <cmath>
 
-auto part_one(std::vector<double>& vec) {
-    return std::transform_reduce(vec.begin(), vec.end(), 0, std::plus<>(), [](auto mass) { return std::floor(mass / 3.0) - 2.0; });
+auto required_fuel(int mass) {
+    return mass / 3 - 2;
 }
 
-auto part_two(std::vector<double>& vec) {
-    return std::transform_reduce(vec.begin(), vec.end(), 0, std::plus<>(), [](auto mass) {
-        auto totalFuel = 0.0;
-
-        while (mass > 0.0) {
-            auto fuel = std::floor(mass / 3.0) - 2.0;
-
-            if (fuel > 0.0) {
-                totalFuel += fuel;
-            }
-
-            mass = fuel;
-        }
-
-        return totalFuel;
-    });
+int fuel_for_fuel(int mass) {
+    auto res = required_fuel(mass);
+    return (res <= 0) ? 0 : res + fuel_for_fuel(res);
 }
 
 int main(void) {
     std::ifstream is("input");
-    std::istream_iterator<double> start(is), end;
+    std::istream_iterator<int> start(is), end;
 
     std::vector modMass(start, end);
 
-    auto fuelRequired = part_one(modMass);
-    auto fuelRequiredToo = part_two(modMass);
+    auto fuelRequired = std::transform_reduce(modMass.begin(), modMass.end(), 0, std::plus<>(), required_fuel);
+    auto fuelRequiredToo = std::transform_reduce(modMass.begin(), modMass.end(), 0, std::plus<>(), fuel_for_fuel);
 
     std::printf("Fuel required: %d\n", fuelRequired);
     std::printf("Fuel required: %d\n", fuelRequiredToo);
